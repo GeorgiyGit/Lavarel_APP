072b6279c9e841449186ca36b7c6943a02366ea0
GeorgiyGit <102670724+GeorgiyGit@users.noreply.github.com>
2023-01-15 11:40:26 +0200

    Add project files.

---
+++ database/migrations/2022_12_25_124913_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {//add model
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();//add product id
+            $table->string('name');//add product name
+            $table->text('description');//add product description
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
